2cfaa73858fc747f9b7cca7c20cd3c06c1f07b90
Prz901 <felipeperozo@hotmail.com>
2019-03-04 15:17:27 -0300

    changes

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const telebot = require('telebot')
-const bot = new telebot('627868396:AAHl1hwhey06dPxahIHpQedetpm5wrlS7V8')
+const bot = new telebot('token')
 
 const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017/'
 
 let user = []
-
+//creat user to db 
 bot.on('/start', (msg) => {
-
-    // colecao de Json 
-    // verificar se ja esta no vetor
-    if (!aaa(user, msg.from.id)) {
+    // verificar se ja esta no banco
+    findUser(msg.from.id).then((resolve) => {
+        console.log(resolve)
+        if (resolve.length > 0) {
+            return bot.sendMessage(msg.from.id, 'usuario ja registrado')
+        }
         insertUser({
             id: msg.from.id,
             username: msg.from.username,
-            name:msg.from.first_name,
+            name: msg.from.first_name,
             events: []
         })
         return bot.sendMessage(msg.from.id, 'usuario registrado ')
-    }
-    return bot.sendMessage(msg.from.id, 'usuario ja registrado')
-})
 
+    }).catch((reject) => {
+        console.log(reject)
+    })
+
+})
 
 bot.on('/luu', (msg) => {
     console.log(user[0].events[0])
@@ -33,35 +37,96 @@ bot.on('/hello', (msg) => {
 })
 
 
-bot.start();
-
-
-const aaa = (arr, id) => {
-    return arr.find((elem) => {
-        if (elem.id === id)
-            return elem
+bot.on('/delete', (msg) => {
+    deleteUser({
+        id: msg.from.id,
     })
-}
+})
+
+bot.start();
 
 bot.on(/^\/adicionarEvento (.+)$/, (msg, props) => {
     const text = props.match[1];
     let eventos = text.split(', ')
-    
-    if(!aaa(user,msg.from.id)){
+
+    if (!aaa(user, msg.from.id)) {
         return bot.sendMessage(msg.from.id, 'voce ainda nao esta registrado, registre-se usando o /start')
     }
-    aaa(user,msg.from.id).events.push({nomeEvento:eventos[0], dia: eventos[1]})
-    return bot.sendMessage(msg.from.id, 'event adicionado ')    
+    aaa(user, msg.from.id).events.push({ nomeEvento: eventos[0], dia: eventos[1] })
+    return bot.sendMessage(msg.from.id, 'event adicionado ')
+});
+bot.on(/^\/changeName (.+)$/, (msg, props) => {
+    const text = props.match[1];
+    console.log(text)
+    updateName(msg.from.id, text)
+    return bot.sendMessage(msg.from.id, 'nome modificado')
 });
 
+// cria o usuario no banco 
 function insertUser(obj) {
     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
-      if (err) throw err
-      var dbo = db.db('calendar')
-      dbo.collection('users').insertOne(obj, function (err, res) {
         if (err) throw err
-        console.log('player inserted')
-        db.close()
-      })
+        const dbo = db.db('calendar')
+        dbo.collection('users').insertOne(obj, function (err, res) {
+            if (err) throw err
+            console.log('player inserted')
+            db.close()
+        })
     })
-  }
+}
+// deleta o usuario do banco
+function deleteUser(obj) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err
+        const dbo = db.db('calendar')
+        dbo.collection('users').deleteOne(obj, function (err, res) {
+            if (err) throw err
+            console.log('deleted')
+            db.close()
+        })
+    })
+}
+// le o usuario no banco
+function findUser(id) {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+            if (err) throw err
+            const dbo = db.db('calendar')
+            const query = { id: id }
+            dbo.collection('users').find(query).toArray((err, result) => {
+                if (err) throw err
+                resolve(result)
+                db.close()
+            })
+        })
+    })
+}
+
+
+function searchName(id){
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+            if (err) throw err
+            const dbo = db.db('calendar')
+            const query = { id: id }
+            dbo.collection('users').find(query).toArray((err, result) => {
+                if (err) throw err
+                resolve(result)
+                db.close()
+            })
+        })
+    })
+}
+
+// update
+function updateName(id, text){
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+        if (err) throw err
+        const dbo = db.db('calendar')
+        const query  = {id: id}
+        dbo.collection('users').updateOne(query, {$set:{'name':text}}, (err, res) => {
+          if (err) throw err
+          db.close()
+        })
+    })
+}
